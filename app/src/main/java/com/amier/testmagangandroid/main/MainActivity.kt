69850ccd20437373f86c94b2e305fb688f743875
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-02-17 19:13:42 +0700

    Initial commit

---
+++ app/src/main/java/com/amier/testmagangandroid/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.amier.testmagangandroid.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amier.testmagangandroid.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
