8c2e7b27de3ff3dd4f216e9f9af7d19433c3f0bb
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-02-18 15:14:25 +0700

    Add-SetupViewModel

--- app/src/main/java/com/amier/testmagangandroid/main/MainFragment.kt
+++ app/src/main/java/com/amier/testmagangandroid/main/MainFragment.kt
@@ -1,30 +1,66 @@
 package com.amier.testmagangandroid.main
 
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.amier.testmagangandroid.R
+import com.amier.testmagangandroid.adapter.MainRVAdapter
 
 import com.amier.testmagangandroid.databinding.FragmentMainBinding
+import com.amier.testmagangandroid.model.MainData
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
-    lateinit var viewbinding: FragmentMainBinding
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+    var list:ArrayList<MainData> = arrayListOf()
+    lateinit var mainD: MainData
+    private lateinit var viewModel: MainViewModel
+    lateinit var mContext: Context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.e("BGT","OPENED")
-
-        viewbinding = FragmentMainBinding.inflate(inflater,container,false).apply {
-
-//            action = object :MainActionListener{
-//                override fun onClickRepos(){
-//                    vm?.openRepo()
-//                }
-//            }
-        }
-        return viewbinding.root
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        mContext = context!!
+        rvRepo.layoutManager = GridLayoutManager(context,3) as RecyclerView.LayoutManager?
+//        rvRepo.layoutManager = StaggeredGridLayoutManager(3,LinearLayoutManager.VERTICAL)
+//        rvRepo.adapter = MainRVAdapter(mContext, dummy)
+        viewModel.setList().observe(viewLifecycleOwner, Observer {
+//            showRecycler(it)
+        })
+    }
+//    fun showRecycler(listArray:ArrayList<MainData>){
+//        rvRepo.adapter = MainRVAdapter(mContext,listArray)
+//    }
+
+//    DUMMY DATA
+    val dummy:ArrayList<MainData> = arrayListOf(
+        MainData("AKU",
+            "https://images2.imgbox.com/52/1b/PMfvPUxo_o.jpg",
+            "https://images2.imgbox.com/0a/e7/G421oy0Q_o.jpg"
+        ),
+        MainData("AKU2",
+            "https://images2.imgbox.com/03/d7/dzXrbOuf_o.jpg",
+            "https://images2.imgbox.com/f1/c5/Tv1L7t9B_o.jpg"
+        ),
+        MainData("AKU3",
+            "https://images2.imgbox.com/3e/4d/p4ip6r31_o.jpg",
+            "https://images2.imgbox.com/59/62/SXRaJdoU_o.jpg"
+        )
+    )
 }
--- app/src/main/java/com/amier/testmagangandroid/main/MainViewModel.kt
+++ app/src/main/java/com/amier/testmagangandroid/main/MainViewModel.kt
@@ -2,8 +2,47 @@ package com.amier.testmagangandroid.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.amier.testmagangandroid.api.NetworkConfig
+import com.amier.testmagangandroid.model.MainData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainViewModel(application: Application):
-    AndroidViewModel(application) {
+class MainViewModel(): ViewModel(){
+    private var data = MutableLiveData<MainData>()
+    private var status = MutableLiveData<Boolean>()
+    private var mutableList = MutableLiveData<ArrayList<MainData>>()
 
+    init {
+        getData(){
+            mutableList = it
+
+        }
+    }
+
+    fun getData(callback: (MutableLiveData<ArrayList<MainData>>) ->Unit) {
+        status.value = true
+        NetworkConfig().api().data().enqueue(object : Callback<ArrayList<MainData>> {
+            override fun onFailure(call: Call<ArrayList<MainData>>, t: Throwable) {
+                status.value = true
+                data.value = null
+            }
+
+            override fun onResponse(call: Call<ArrayList<MainData>>, response: Response<ArrayList<MainData>>) {
+                status.value = false
+
+                if (response.isSuccessful){
+                    mutableList.value = response.body()
+                    callback(mutableList)
+                }
+                else
+                    status.value = true
+            }
+        })
+    }
+    fun setList():MutableLiveData<ArrayList<MainData>>{
+        return mutableList
+    }
 }
