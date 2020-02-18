bb22ee341e401e21cb078a9ef7a3dbc320ce947d
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-02-18 15:23:26 +0700

    bug fixes

--- app/src/main/java/com/amier/testmagangandroid/main/MainBinding.kt
+++ app/src/main/java/com/amier/testmagangandroid/main/MainBinding.kt
@@ -1,4 +1,16 @@
 package com.amier.testmagangandroid.main
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.amier.testmagangandroid.adapter.MainRVAdapter
+import com.amier.testmagangandroid.model.MainData
+
 object MainBinding {
+    @BindingAdapter("app:mainList")
+    @JvmStatic
+    fun setRepoList(recyclerView: RecyclerView, repoData: ArrayList<MainData>) {
+        with(recyclerView.adapter as MainRVAdapter) {
+            replaceData(repoData)
+        }
+    }
 }
